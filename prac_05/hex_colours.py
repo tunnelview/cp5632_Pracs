91f45d547e15c9182cdeb88e635d252f4b476756
tunnelview <86519065+tunnelview@users.noreply.github.com>
2021-09-03 14:26:32 +1000

    Improve naming in hex_colours

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -4,13 +4,13 @@ COLOUR_CODE = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7",
                "azure4": "#838b8b", "black": "#000000",
                "blue2": "#0000ee", "BlueViolet": "#8a2be2", "brown4": "#8b2323"}
 
-COLOUR_NAME = input("Please enter your colour name: ")
-while COLOUR_NAME != "":
-    if COLOUR_NAME in COLOUR_CODE:
-        print(COLOUR_NAME, "is", COLOUR_CODE[COLOUR_NAME])
+colour_name = input("Please enter your colour name: ")
+while colour_name != "":
+    if colour_name in COLOUR_CODE:
+        print(colour_name, "is", COLOUR_CODE[colour_name])
     else:
-        print("Invalid Colur Name!")
-    COLOUR_NAME = input("Please enter your colour name: ")
+        print("Invalid Colour Name!")
+    colour_name = input("Please enter your colour name: ")
 
 
 
