34c77299d63874fdf0fa37cd026dd36a8d6f1bb1
tunnelview <86519065+tunnelview@users.noreply.github.com>
2021-10-30 23:49:50 +1000

    Add better naming to wiki.py

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,10 +1,10 @@
 import wikipedia
 
-search_page_title = input("Please enter the page title you want to search: ")
-while search_page_title != "":
-    print(wikipedia.search(search_page_title))
-    print(wikipedia.summary(search_page_title))
-    search_page_title = input("Please enter the page title you want to search: ")
+page_title = input("Please enter the page title you want to search: ")
+while page_title != "":
+    print(wikipedia.search(page_title))
+    print(wikipedia.summary(page_title))
+    page_title = input("Please enter the page title you want to search: ")
 
 
 
